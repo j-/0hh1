264de11b807a24fe883197b213f9b8bd755e90f0
j- <j@skeoh.com>
2018-09-24 14:06:47 +1000

    Add "combine ranges" static method of grid range

--- src/grid-range.ts
+++ src/grid-range.ts
@@ -8,6 +8,16 @@ export class GridRange<T = any> implements Iterable<GridItem<T>> {
 	readonly length: number;
 	[index: number]: GridItem<T>;
 
+	public static combineRanges<T = any> (left: GridRange<T>, right: GridRange<T>): GridRange<T> {
+		return new GridRange(
+			Math.min(left.x1, right.x1),
+			Math.min(left.y1, right.y1),
+			Math.min(left.x2, right.x2),
+			Math.min(left.y2, right.y2),
+			[...Array.from(left), ...Array.from(right)],
+		);
+	}
+
 	constructor (x1: number, y1: number, x2: number, y2: number, items: GridItem<T>[]) {
 		for (let i = 0; i < items.length; i++) {
 			Object.defineProperty(this, i, {
